add2278d214be14112f6a28087dd38c2a097e3e1
Mikaeel <95021467+Mikaeelmoha123@users.noreply.github.com>
2022-12-04 04:10:03 -0800

    Add files via upload

---
+++ bankbalance.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age;
+    float bankBalance;
+    cout << "Enter your age: ";
+    cin >> age;
+    if (age >= 22)
+    {
+        cout << "Proceed to key in bank balance: ";
+        else if (age < 22)
+                cin >>
+            bankBalance;
+        if (bankBalance > 50, 000)
+    }
+}
